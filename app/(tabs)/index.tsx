55dfb651c2a7bb90d69bb6bfe56fbe0fb06af964
Nhat Cong Duong <nhat.duong@asnet.com.vn>
2025-11-06 09:27:49 +0700

    Resolve Implement Button component with multiple variants and loading state

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import { Text } from '@/components/ui';
+import { Button } from '@/components/ui/Button';
 import { View } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
 
@@ -6,6 +7,10 @@ export default function HomeScreen() {
   return (
     <View style={styles.container}>
       <Text>home</Text>
+      <Button variant="outline">Button label</Button>
+      <Button variant="secondary" loading>
+        Loading...
+      </Button>
     </View>
   );
 }
